89321ca69369d09f40ef78fc6d2ddadd6e5866ba
Sakat Aziz Khan <sakatazizkhan1@gmail.com>
2025-01-17 16:35:18 +0530

    updated plugin meta

--- hyper-checkout-for-woocommerce.php
+++ hyper-checkout-for-woocommerce.php
@@ -1,11 +1,23 @@
 <?php
 /**
- * Plugin Name: Hyper Checkout for WooCommerce
- * Description: Create special checkout links that add products, apply discounts, enable free shipping, and apply conditions.
- * Version: 1.0.0
- * Author: Aziz Khan
- * Text Domain: hyper-checkout-for-woocommerce
+ * Plugin Name:       Hyper Checkout for WooCommerce
+ * Plugin URI:        https://gethypercheckout.com
+ * Description:       Create special checkout links that add products, apply discounts, enable free shipping, and apply conditions for a seamless checkout experience.
+ * Version:           1.0.0
+ * Author:            Hypertics AI
+ * Author URI:        https://HyperticsAI.com
+ * Text Domain:       hyper-checkout-for-woocommerce
+ * Domain Path:       /languages
+ * Requires at least: 5.0
+ * Requires PHP:      7.4
+ * Tested up to:      6.4
+ * Requires Plugins: woocommerce
+ * WC requires at least: 4.0
+ * WC tested up to:   8.0
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  */
+
 // File location: hyper-checkout-for-woocommerce.php
 if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
